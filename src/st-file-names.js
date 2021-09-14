48dadad708ec7d65334ed3413ba1dfd519839a2e
Pavel <pavel.priladyshev@gmail.com>
2021-09-14 12:16:28 +0300

    feat: st-file-names done

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,7 +15,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  const n = names;
+  let count = 0;
+  for (let j = 0; j < n.length; j++) {
+    for (let i = 0; i < j; i++) {
+      if (n[i] === n[i + j]) {
+        count++;
+        if (n[i].slice(-1) === ")") {
+          count--;
+        } else {
+          n[i + j] = n[i + j].concat(`(${count})`);
+          count++;
+        }
+        if (n[i].slice(-1) === ")") {
+          count--;
+          n[i + j] = n[i + j].concat(`(${count})`);
+        }
+      }
+    }
+  }
+  return n;
 }

c39c7a318fd7ed1c4a07cb1420c21f4b75025318
Pavel <pavel.priladyshev@gmail.com>
2022-05-01 10:46:31 +0300

    feat: delete digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -15,9 +15,21 @@ function deleteDigit(n) {
   let arr = [];
   let numArray = n.toString().split("").map(Number);
   let min = Math.min(...numArray);
+  let max = Math.max(...numArray);
+  if (numArray.length === 2) {
+    arr.push(numArray[0]);
+    return Number(arr);
+  }
   for (let i = 0; i < numArray.length; i++) {
-    if (numArray[i] === min) {
-      i++;
+    if (numArray[i] < max) {
+      if (numArray[i] < numArray[i + 1]) {
+        i++;
+      }
+      max = -1;
+    } else if (numArray[i] === min) {
+      if (numArray[i] < numArray[i + 1]) {
+        i++;
+      }
       min = -1;
     }
     arr.push(numArray[i]);

3d836fc44a89084266a9f3c500a422cf4116c2d9
Pavel <pavel.priladyshev@gmail.com>
2022-05-01 10:07:45 +0300

    feat: dns stats & encode line

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let arr = [];
+  let numArray = n.toString().split("").map(Number);
+  let min = Math.min(...numArray);
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i] === min) {
+      i++;
+      min = -1;
+    }
+    arr.push(numArray[i]);
+  }
+  return Number(arr.join(""));
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  if (!Array.isArray(domains)) {
+    return false;
+  }
+  let res = {};
+  for (let i = 0; i < domains.length; i++) {
+    let temp = domains[i].split(".").reverse();
+    let domain = "";
+    for (let j = 0; j < temp.length; j++) {
+      domain += `.${temp[j]}`;
+      res[domain] = (res[domain] || 0) + 1;
+    }
+  }
+  return res;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
